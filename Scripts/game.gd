9439a4f2c36cb3b56299d67dfde72f16c9d68c32
Alethium <132704680+Alethium@users.noreply.github.com>
2025-12-27 12:24:41 -0800

    basic map
    tileset created

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -5,3 +5,7 @@ extends Control
 func _ready() -> void:
 	$HBoxContainer/Player_2/SubViewport.world_2d = $HBoxContainer/Player_1/player_1_view.find_world_2d()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().reload_current_scene()
